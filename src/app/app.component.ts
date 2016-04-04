6b0a9f4fea3824d9f3c10d77b03c42045618e9ac
William Welling <wwelling@outlook.com>
2016-04-03 19:19:41 -0500

    Refactored navbar title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ import {ItemComponent} from './dspace/components/item.component';
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span> 
                             </button>
-                            <a class="navbar-brand" [routerLink]="['/Home']">TAMU DSpace UI</a>
+                            <a class="navbar-brand" [routerLink]="['/Home']">DSpace UI</a>
                         </div>
                         <div class="collapse navbar-collapse" id="myNavbar">
                             <ul class="nav navbar-nav">
