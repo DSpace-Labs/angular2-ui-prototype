7acb1fda363a0ac9adaa20eb6ed3ae59c9c29c3a
dylan <dylan.meeus@atmire.com>
2016-04-11 10:20:15 +0200

    Truncation pipe

---
+++ src/app/utilities/pipes/truncate.pipe.ts
@@ -0,0 +1,17 @@
+import {Pipe} from 'angular2/core'
+
+/**
+ * Pipe to truncate a value in Angular2. (Take a substring, starting at 0)
+ * Default value: 10
+ *
+ */
+
+@Pipe({
+    name: 'truncate'
+})
+export class TruncatePipe {
+    transform(value: string, args: string[]) : string {
+        let limit = args.length > 0 ? parseInt(args[0], 10) : 10; // 10 as default truncate value
+        return value.length > limit ? value.substring(0, limit): value;
+    }
+}
