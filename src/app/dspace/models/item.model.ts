a5ae69639a13f27820a46be3a19eb58bd91fe6cf
dylan <dylan.meeus@atmire.com>
2016-04-20 16:54:21 +0200

    Remove typo

--- src/app/dspace/models/item.model.ts
+++ src/app/dspace/models/item.model.ts
@@ -11,7 +11,7 @@ export class Item extends DSOContainer {
 
     bitstreams : Array<Bitstream> = [];
 
-    parentCollection : Collection;i
+    parentCollection : Collection;
 
     lastModified: string; //TODO: change to date, deserialize
 
