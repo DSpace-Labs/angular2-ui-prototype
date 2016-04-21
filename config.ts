1dfc6f1dd5cf2bf3f19b97f8260bfd0c1fd53bcd
dylan <dylan.meeus@atmire.com>
2016-04-21 14:16:54 +0200

    oaktrust instead of demo.dspace

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 let GlobalConfig = {
 	dspace: {
 		root: '/rest',
-		url: 'demo.dspace.org'
+		url: 'http://oaktrust.library.tamu.edu'
 	},
 	proxy: 'http://localhost:5050',
 	host: 'http://localhost:3000'
