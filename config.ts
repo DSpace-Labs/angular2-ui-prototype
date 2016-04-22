fe736ff5bcf9b0d938802b2e30308ad950911988
Art Lowel <art@ArtMac.local>
2016-04-22 12:03:17 +0200

    fixed proxy url

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ const GlobalConfig = {
 	rest: {
 		//Space is capitalized because 'namespace' is a reserved string in TypeScript
 		nameSpace: '/rest',
-		baseURL: 'http://localhost:5050'
+		baseURL: 'http://oaktrust.library.tamu.edu'
 	},
 	proxy: {
 		nameSpace: '/',
