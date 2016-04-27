440ab8423c74988d2838f1f13d387dd30f1f2c0c
dylan <dylan.meeus@atmire.com>
2016-04-27 15:40:59 +0200

    Showing new item-list in the collections

---
+++ src/app/dspace/components/item-list.component.ts
@@ -0,0 +1,61 @@
+import {Component, Input} from 'angular2/core';
+import {RouteParams} from 'angular2/router';
+import {TranslatePipe} from "ng2-translate/ng2-translate";
+
+import {DSpaceDirectory} from '../dspace.directory';
+import {DSpaceService} from '../services/dspace.service';
+
+import {ListEntryComponent} from './item/list/list-entry.component';
+import {Item} from '../models/item.model';
+import {Collection} from '../models/collection.model';
+import {PaginationComponent} from '../../navigation/components/pagination.component';
+
+/**
+ * Renders a row of the item list.
+ */
+@Component({
+    selector: 'item-list',
+    directives: [ListEntryComponent],
+    inputs: ['items', 'collection'],
+    template:
+        `
+            <div *ngFor="#item of itemsWithInformation" id="list-entries" class="row item-list-row">  <!-- for each item, we create an item-list-entry element -->
+                <list-entry [item]="item"></list-entry>
+            </div>
+        `
+})
+
+
+
+export class ItemListComponent {
+
+    collection : Collection;
+    items : Item[]; // pass an array of items to this component.
+    itemsWithInformation : Item[] = []; // Loaded the metadata of the item and storing them here.
+    constructor(private directory: DSpaceDirectory)
+    {
+    }
+
+    ngOnChanges()
+    {
+        if(this.collection != null)
+        {
+            // map the component to these items.
+            // we need to load extra metadata of all these items, because we want to show more than what the dspace API returns by default
+            console.log("in the collection");
+            console.log(this.collection.items);
+            setTimeout( () => console.log(this.collection.items),10000);
+            this.collection.items.forEach( (entry) =>
+            {
+                console.log("for each item");
+                this.directory.loadObj('item',entry.id,0).then( (item:Item) =>
+                {
+                    console.log("loaded information");
+                    this.itemsWithInformation.push(item);
+                });
+            });
+        }
+    }
+
+
+}
---
+++ src/app/dspace/components/item/list/list-entry.component.ts
@@ -0,0 +1,30 @@
+import {Component, Input} from 'angular2/core';
+import {TranslatePipe} from "ng2-translate/ng2-translate";
+
+import {ListMetadataComponent} from './list-metadata.component';
+import {ThumbnailComponent} from '../thumbnail.component';
+import {Item} from '../../../models/item.model';
+
+
+@Component({
+    selector: 'list-entry',
+    inputs: ['item'],
+    directives:[ListMetadataComponent, ThumbnailComponent],
+    pipes: [TranslatePipe],
+    template:
+        `
+            <div class="col-sm-2 hidden-xs">
+            <!-- need to replace these with holder.js images -->
+               <item-thumbnail></item-thumbnail>
+            </div>
+            <div class="col-sm-8 col-xs-12">
+                 <item-list-metadata [item]="item"></item-list-metadata>
+            </div>
+            `
+})
+
+export class ListEntryComponent
+{
+    item : Item;
+}
+
