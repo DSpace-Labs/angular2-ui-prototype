541060c317b29be32c25259d5c96877c5c932275
William Welling <wwelling@outlook.com>
2016-05-07 08:16:01 -0500

    Caching json forms.

--- src/app/utilities/form/form.service.ts
+++ src/app/utilities/form/form.service.ts
@@ -15,24 +15,42 @@ import { FormInput } from './form-input.model';
 export class FormService {
 
     /**
+     * 
+     */
+    private formStore: Map<string, Array<FormInput>>;
+
+    /**
      * @param httpService 
      *      HttpService is a singleton service to provide basic xhr requests.
      */
-    constructor(private httpService: HttpService) {}
+    constructor(private httpService: HttpService) {
+        this.formStore = new Map<string, Array<FormInput>>();
+    }
 
     /**
      * Get the metadata input form json.
      */
     getForm(form: string): Observable<Array<FormInput>> {
-        return this.httpService.get({
-            url: URLHelper.relativeToAbsoluteUIURL('/static/forms/' + form + '.json')
-        }).map(json => {
-            let inputs = new Array<FormInput>();
-            for(let input of json) {
-                inputs.push(new FormInput(input));
-            }
-            return inputs;
-        });
+        let inputs = this.formStore.get(form);
+        if(inputs) {
+            console.log('already have form')
+            return Observable.create(observer => {
+                observer.next(inputs);
+                observer.complete();
+            });
+        }
+        else {
+            return this.httpService.get({
+                url: URLHelper.relativeToAbsoluteUIURL('/static/forms/' + form + '.json')
+            }).map(json => {
+                inputs = new Array<FormInput>();
+                for(let input of json) {
+                    inputs.push(new FormInput(input));
+                }
+                this.formStore.set(form, inputs);
+                return inputs;
+            });
+        }
     }
     
     /**
