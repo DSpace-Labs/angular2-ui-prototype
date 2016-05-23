f83c72800815c102d1b743e2416a9e97e230d895
dylan <dylan.meeus@atmire.com>
2016-05-23 13:51:39 +0200

    basic sidebar edits

--- src/app/dspace/components/sidebar/admin-sidebar.component.ts
+++ src/app/dspace/components/sidebar/admin-sidebar.component.ts
@@ -4,6 +4,7 @@ import { ROUTER_DIRECTIVES, RouteConfig, Router } from '@angular/router-deprecat
 import { ContextProviderService } from '../../../dspace/services/context-provider.service';
 import {SidebarService} from '../../../utilities/services/sidebar.service.ts';
 import {SidebarSection} from '../../models/sidebar/sidebar-section.model';
+import { HrefSidebarSection } from '../../models/sidebar/hrefsidebar-section.model';
 import {SidebarSectionComponent} from './sidebar-section.component';
 /**
  * Main component to render the sidebar. Will access the sidebarservice to find out how much components need to be rendered.
@@ -16,6 +17,18 @@ import {SidebarSectionComponent} from './sidebar-section.component';
         `
             <h1>Edit the sidebar</h1>
             <!-- show the sidebar here, with some added options (to add links) -->
+
+            <form id="addsidebarsection">
+
+            <fieldset>
+                <label>Section name:<input  [(ngModel)]="sectionName"  type="text"/></label>
+            </fieldset>
+
+                <label>Url:<input [(ngModel)]="sectionUrl" type="text"/></label>
+                <label>Index:<input [(ngModel)]="sectionIndex" type="text"/></label>
+                <button value="Add" (click)="addSection()">Add</button>
+            </form>
+
         `
 })
 
@@ -25,10 +38,29 @@ import {SidebarSectionComponent} from './sidebar-section.component';
  */
 export class AdminSidebarComponent
 {
+    // at the moment we can only add the hrefsidebar
 
-    constructor()
+    sectionName : string;
+    sectionUrl : string;
+    sectionIndex : number; // with a fancy UI, we could have the user just click somewhere in the sidebar.
+
+
+    constructor(private sidebarService : SidebarService)
     {
-        console.log("in the admin sidebar component");
+        // we name one section
+        // the section can have multiple children
     }
 
+
+    addSection()
+    {
+        console.log("section name: " + this.sectionName);
+        // create new sidebar based on the data that I got here.
+        // will need to switch over the ones that are set.
+        // or I can pass undefined and have the builder check these thigns?
+        let sidebarComponent = HrefSidebarSection.getBuilder()
+                                .url(this.sectionName,this.sectionUrl).index(this.sectionIndex)
+                                .name("test").id(this.sectionName).build(); // generate a section id oursevles?
+        this.sidebarService.addSection(sidebarComponent);
+    }
 }
