ac5bb0cf7448a41672fb2c91539d5455257af4ce
dylan <dylan.meeus@atmire.com>
2016-05-27 16:20:54 +0200

    remove comments

--- src/app/dspace/components/sidebar/admin-sidebar.component.ts
+++ src/app/dspace/components/sidebar/admin-sidebar.component.ts
@@ -1,10 +1,10 @@
-import {Component, Input, OnInit, EventEmitter, Output} from '@angular/core';
+import {Component } from '@angular/core';
 import { ROUTER_DIRECTIVES, RouteConfig, Router } from '@angular/router-deprecated';
 
-import { ContextProviderService } from '../../../dspace/services/context-provider.service';
 import { SidebarService } from '../../../utilities/services/sidebar.service.ts';
 import { SidebarSection } from '../../models/sidebar/sidebar-section.model';
 import { SidebarSectionComponent } from './sidebar-section.component';
+import { ArrayUtil } from "../../../utilities/commons/array.util";
 /**
  * Main component to render the sidebar. Will access the sidebarservice to find out how much components need to be rendered.
  * Using the sidebarservice
@@ -30,7 +30,7 @@ import { SidebarSectionComponent } from './sidebar-section.component';
                         <div *ngFor="let child of entry.childsections let i = index" class="panel-body">
                             <label>Name <input class="form-control" [(ngModel)]="child.componentName" required type="text"/></label>
                             <label>Url <input class="form-control" [(ngModel)]="child.url" required type="text"/></label>
-                            <label>Id <input class="form-control" [(ngModel)]="child.id"/></label>
+                            <input type="hidden" class="form-control" [(ngModel)]="child.id"/>
                             <!-- to be implemented later
                             <label>
                                 <input type="checkbox"> Public?
@@ -39,8 +39,12 @@ import { SidebarSectionComponent } from './sidebar-section.component';
                            
                             <!-- we only show the 'plus symbol' on the first entry -->
                             <span *ngIf="i==0" class="glyphicon glyphicon-plus clickable" aria-hidden="true" (click)="addChildSectionField(entry)"></span>
-                            <span *ngIf="i>0" class="glyphicon glyphicon-remove clickable" aria-hidden="true" (click)="removeChildSection(entry,i)"></span>
+                            <span class="glyphicon glyphicon-remove clickable" aria-hidden="true" (click)="removeChildSection(entry,i)"></span>
                         </div>
+                        
+                        <!-- if there are no children o the current component, we still want to show an add button to add the first child -->
+                        <button *ngIf="!hasChildren(entry)" type="button" class="btn btn-primary btn-sm" (click)="addChildSectionField(entry)">Add child section</button>        
+                        
                 </div>
             <!-- buttons here -->
             <div id="controls">
@@ -72,15 +76,10 @@ export class AdminSidebarComponent
         this.populateForm();
     }
 
-    // populate with the sections that already exist.
-    // in the future needs to be loeaded from a file.
     populateForm()
     {
         let customSections = this.sidebarService.getTopSections();
         this.entries = customSections.slice(0);
-
-        // now we populate our system based on this.
-        // one section per top-level, put the children underneath this
     }
 
     addSectionField()
@@ -91,19 +90,16 @@ export class AdminSidebarComponent
         let parentSection = SidebarSection.getBuilder().id(generatedId).build();
         this.addChildSectionField(parentSection);
         this.sidebarService.addSection(parentSection);
-        // give this parent a child as well.
         this.entries = this.sidebarService.getTopSections().slice(0);
     }
 
 
     addChildSectionField(parent : SidebarSection)
     {
-        // need to assign a random ID to the child section. Maybe also store this in the input as a hidden field?
         let generateId : string = "custom-child-section-" + new Date().getTime();
         parent.childsections.push(SidebarSection.getBuilder().id(generateId).build());
     }
 
-    // this actually removed the section
     removeChildSection(parent, child)
     {
         parent.childsections.splice(child,1);
@@ -121,18 +117,11 @@ export class AdminSidebarComponent
         console.log("saving the edits to the json file");
     }
 
+    hasChildren(parent) : boolean
+    {
+        console.log("checking for children");
+        console.log(parent);
+        return ArrayUtil.isNotEmpty(parent.childsections);
+    }
 }
-
-/*
-class SidebarEntry
-{
-
-    // a top-level only has a name, not a url. That is one way to notice the difference.
-    // but the children need to be attached to the parent somehow.
-    sectionName : string;
-    sectionUrl : string;
-    sectionIndex : number; // with a fancy UI, we could have the user just click somewhere in the sidebar.
-
-}
-    */
 
